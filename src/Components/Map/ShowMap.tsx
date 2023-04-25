3e8d6def213f728ad10e46d6b3081830cbc69488
Connor Mackay <connor.mackay@gmail.com>
2023-04-25 19:45:12 -0400

    Color code markers according to risk factor

--- src/Components/Map/ShowMap.tsx
+++ src/Components/Map/ShowMap.tsx
@@ -3,11 +3,11 @@ import { useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import MarkerContainer from "../MarkerContainer/MarkerContainer";
 
-interface Props {
+interface ShowMapProps {
   dataset: Array<string>;
-};
+}
 
-const ShowMap = ({ dataset }: Props) => {
+const ShowMap = ({ dataset }: ShowMapProps) => {
   const [geoData, setGeoData] = useState({ lat: 53.51684, lng: -113.3187 });
 
   const center: [number, number] = [geoData.lat, geoData.lng];
@@ -22,9 +22,7 @@ const ShowMap = ({ dataset }: Props) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {geoData.lat && geoData.lng && <MarkerContainer 
-      dataset={dataset} 
-      />}
+      {geoData.lat && geoData.lng && <MarkerContainer dataset={dataset} />}
     </MapContainer>
   );
 };
--- src/Components/MarkerContainer/MarkerContainer.tsx
+++ src/Components/MarkerContainer/MarkerContainer.tsx
@@ -2,19 +2,30 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { Marker, Popup } from "react-leaflet";
 
-type Props = {
+interface MarkerContainerProps {
   dataset: Array<string>;
 };
 
-const MarkerContainer = ({ dataset }: Props) => {
+const MarkerContainer = ({ dataset }: MarkerContainerProps) => {
 
   const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
+
+  const handleIconColor = (input:number) => {
+    if (input < 0.5) {
+      return L.icon({ iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|abcdef&chf=a,s,ee00FFFF" }) 
+    } else {
+      return L.icon({ iconUrl: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2ecc71&chf=a,s,ee00FFFF"}) 
+    }
+  }
+
 
   const showMarkers = (input: Array<string>) => {
     return input.map((element, index) => {
       return (
-        <Marker key={index} position={[Number(element[1]), Number(element[2])]} icon={icon}>
-          <Popup key={index}>A popup</Popup>
+        <Marker key={index} position={[Number(element[1]), Number(element[2])]} icon={handleIconColor(Number(element[4]))}>
+          <Popup key={index}>
+            {element[4]}
+          </Popup>
         </Marker>
       );
     });
