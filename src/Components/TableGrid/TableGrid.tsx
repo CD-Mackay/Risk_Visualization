d1f965fd579b4832ca6bf74324ec680dd5fbac3a
Connor Mackay <connor.mackay@gmail.com>
2023-04-28 18:15:53 -0400

    add greatest risk field to tablegrid

--- src/Components/TableGrid/TableGrid.tsx
+++ src/Components/TableGrid/TableGrid.tsx
@@ -1,40 +1,32 @@
 import Paper from "@mui/material/Paper";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import "leaflet/dist/leaflet.css";
-import { useState } from "react";
 
 interface TableGridProps {
   dataset: Array<string>;
+  chartParam: {
+    param: string;
+    currentValue: string;
+  };
 }
 
-const TableGrid = ({ dataset }: TableGridProps) => {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-
-  // Implement filter functionality on reasonable columns, especially risk factors.
-
-  const handlePageSelect = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
+const TableGrid = ({ dataset, chartParam }: TableGridProps) => {
   function createData(
     id: number,
     assetName: string,
-    // lat: string,
-    // lng: string,
     businessCategory: string,
     riskRating: number,
     riskFactors: string,
+    greatestRisk: string,
     year: number
   ) {
     return {
       id,
       assetName,
-      // lat,
-      // lng,
       businessCategory,
       riskRating,
       riskFactors,
+      greatestRisk,
       year,
     };
   }
@@ -49,34 +41,43 @@ const TableGrid = ({ dataset }: TableGridProps) => {
       type: "number",
     },
     { field: "riskFactors", headerName: "Risk Factors", width: 600 },
+    { field: "greatestRisk", headerName: "Greatest Risk(s)", width: 400 },
     { field: "year", headerName: "Year", width: 60, type: "number" },
   ];
 
   const rows = dataset.map((element, index) => {
     const riskData = JSON.parse(element[5]);
+    var riskArr = Object.keys( riskData ).map(function ( key ) { return riskData[key]; });
+    let max = Math.max.apply(null, riskArr);
+    let greatest: any = {}
     for (let element in riskData) {
       if (riskData[element] === 0.0) {
-        delete riskData[element]
+        delete riskData[element];
+      }
+      if (riskData[element] === max) {
+        greatest[element] = max;
       }
     }
-    const showRisk = Object.keys(riskData).join(",")
+    const showRisk = Object.keys(riskData).join(",");
+    let renderGreatest = JSON.stringify(greatest);
     return createData(
       index,
       element[0],
-      // element[1],
-      // element[2],
       element[3],
       Number(element[4]),
-      showRisk, // Convert data type to eliminate string quotes on object
+      showRisk,
+      renderGreatest,
       Number(element[6]) // remove number conversion
     );
   });
 
   return (
     <Paper>
+      <h3>
+        Risk Data by {chartParam.param} for {chartParam.currentValue}
+      </h3>
       <DataGrid
         rows={rows}
-        // paginationModel={{ page: 0, pageSize: 10 }}
         columns={columns}
         initialState={{
           pagination: { paginationModel: { pageSize: 5 } },
