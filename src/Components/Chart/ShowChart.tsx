b9a01f5961f646d8c52f4b9a50fb5c50631b53c3
Connor Mackay <connor.mackay@gmail.com>
2023-04-27 21:01:30 -0400

    implement basic chart

--- src/Components/Chart/ShowChart.tsx
+++ src/Components/Chart/ShowChart.tsx
@@ -1,11 +1,50 @@
+import { Chart, registerables } from "chart.js";
+import { Line } from "react-chartjs-2";
+
+
 interface ShowChartProps {
   dataset: Array<string>;
   headers: Array<string>;
 }
 
 const ShowChart = ({ dataset, headers }: ShowChartProps) => {
-  console.log(dataset,headers);
-  return <div>{/* <Line /> */}</div>;
+  const risks = JSON.parse(dataset[0][5]);
+  Chart.register(...registerables);
+
+  console.log("risks", risks);
+  const chartHeaders = Object.keys(risks);
+  const chartData = Object.values(risks);
+  console.log("data!", chartData, chartHeaders);
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+
+  const data = {
+    labels: chartHeaders,
+    datasets: [
+      {
+        label: dataset[0],
+        data: chartData,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Line options={options} data={data} />
+    </div>
+  );
 };
 
 export default ShowChart;
